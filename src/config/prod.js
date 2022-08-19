a18559c7c3dad1a50f1bdeb391a8a1c2a6a5b311
gianpietro1 <gianpietro1@gmail.com>
2022-08-18 22:27:39 -0500

    fixes

---
+++ src/config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: process.env.DBHOST || "localhost",
+  user: process.env.USERDB || "root",
+  password: process.env.PASSWORDDB || "root",
+  database: process.env.DBNAME || "db",
+  port: process.env.DBPORT || "3306"
+};
