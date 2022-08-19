133362ce57f24a671f5912d17c13eed240146990
gianpietro1 <gianpietro1@gmail.com>
2022-08-18 22:33:58 -0500

    fixes

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,7 +1,8 @@
 module.exports = {
-  host: process.env.DBHOST || "localhost",
-  user: process.env.USERDB || "root",
-  password: process.env.PASSWORDDB || "root",
-  database: process.env.DBNAME || "db",
-  port: process.env.DBPORT || "3306"
+  mongoURI: process.env.MONGO_URI,
+  host: process.env.DBHOST || 'localhost',
+  user: process.env.USERDB || 'root',
+  password: process.env.PASSWORDDB || 'root',
+  database: process.env.DBNAME || 'db',
+  port: process.env.DBPORT || '3306',
 };
